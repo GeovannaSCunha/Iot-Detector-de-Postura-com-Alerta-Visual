9420f7e978ebb3b8801a84080894a70b8c4373e7
Geovanna Cunha <126582620+GeovannaSCunha@users.noreply.github.com>
2025-04-24 09:18:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_con
 
 
 # Configurações
-LIMIAR_INCLINACAO = 0.08  # Ajuste este valor (0.05 a 0.15)
+LIMIAR_INCLINACAO = 0.08  # (0.05 a 0.15)
 POSTURA_RUIM_TEMPO = 0    # Contador de tempo em postura ruim
 
 cap = cv2.VideoCapture(0)  # Webcam ou video gravado .mp4
@@ -30,8 +30,8 @@ while True:
         for face_landmarks in results.multi_face_landmarks:
             # Obtém os pontos do nariz (1) e queixo (152)
             pontos = face_landmarks.landmark
-            nariz = [pontos[1].x, pontos[1].y]  # Landmark 1
-            queixo = [pontos[152].x, pontos[152].y]  # Landmark 15
+            nariz = [pontos[1].x, pontos[1].y] 
+            queixo = [pontos[152].x, pontos[152].y]  
 
             # Calcula a diferença vertical normalizada (0-1)
             diferenca = abs(nariz[1] - queixo[1])
@@ -57,7 +57,7 @@ while True:
             cv2.circle(frame, (int(nariz[0] * w), int(nariz[1] * h)), 5, (0, 255, 0), -1)
             cv2.circle(frame, (int(queixo[0] * w), int(queixo[1] * h)), 5, (0, 0, 255), -1)
 
-    cv2.imshow("Detector de Postura Simplificado", frame)
+    cv2.imshow("Detector de Postura ao mexer no celular", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
